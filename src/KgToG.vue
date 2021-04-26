e08467f2b962d9e5c122ad442cf5f4ddcf6ba245
Smettraux <mettrauxsteve@gmail.com>
2021-04-26 15:45:25 +0200

    temp converter added

---
+++ src/KgToG.vue
@@ -0,0 +1,25 @@
+<script>
+  export default {
+    data() {
+      return {
+        kg: 1
+      }
+    },
+
+    computed: {
+      g() {
+        return this.kg * 1000;
+      },
+      isHeavy() {
+        return this.kg >= 100;
+      }
+    },
+
+    methods: {
+      updateKg(event) {
+        this.kg = event.target.value;
+      }
+    }
+
+  }
+</script>
---
+++ src/KgToGSetup.vue
@@ -0,0 +1,28 @@
+<script>
+import { ref, computed } from "vue";
+
+export default {
+  setup(props) {
+    const kg = ref(0);
+    const g = computed(() => kg.value * 1000);
+    const updateKg = (event) => (kg.value = event.target.value);
+    return { kg, g, updateKg };
+  },
+};
+</script>
+
+<template>
+  <div>
+    <input @input="updateKg" type="number" :value="kg"> [Kg] 
+  </div>
+  <div>
+    {{ g }} [g]
+  </div>
+  <span v-if="kg > 1000"> C'est lourd !</span>
+</template>
+
+<style scoped>
+span {
+  color: tomato;
+}
+</style>
